0b6d59b0db25a788a521bc1f879b0dfe6ed16781
summer0512 <summer051200@gmail.com>
2025-09-01 22:10:35 +0800

    feat: analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,5 +7,19 @@ type Props = {
 // Since we have a `not-found.tsx` page on the root, a layout file
 // is required, even if it's just passing children through.
 export default function RootLayout({children}: Props) {
-  return children;
+  return(
+    <>
+      {children}
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-C3W52QVV6K"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){
+          dataLayer.push(arguments)
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-C3W52QVV6K');
+      </script>
+    </>
+    )
 }
