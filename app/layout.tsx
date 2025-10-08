67c1eecc2d42625d9ece49cfeb3d281238a0413e
summer0512 <summer051200@gmail.com>
2025-10-08 19:28:59 +0800

    feat: ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import {ReactNode} from 'react';
+﻿import {ReactNode} from 'react';
 import Script from 'next/script';
 
 type Props = {
@@ -10,14 +10,19 @@ type Props = {
 export default function RootLayout({children}: Props) {
   return(
     <>
-      {children}
       <Script
         strategy="afterInteractive"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2623631636848395"
+        crossOrigin="anonymous"
+      />
+      {children}
+      <Script
+        strategy="lazyOnload"
         src="https://www.googletagmanager.com/gtag/js?id=G-C3W52QVV6K"
       />
       <Script
         id="google-analytics"
-        strategy="afterInteractive"
+        strategy="lazyOnload"
         dangerouslySetInnerHTML={{
           __html: `
             window.dataLayer = window.dataLayer || [];
