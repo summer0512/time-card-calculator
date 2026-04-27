eba2cdaca029b57a771bc05f3b227506d401effa
summer0512 <summer051200@gmail.com>
2026-04-27 08:48:24 +0800

    ga

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,30 +8,35 @@ type Props = {
 // Since we have a `not-found.tsx` page on the root, a layout file
 // is required, even if it's just passing children through.
 export default function RootLayout({children}: Props) {
+  const isProduction = process.env.NODE_ENV === 'production';
+
   return(
     <>
-      <Script
+      { isProduction && <Script
         strategy="afterInteractive"
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2623631636848395"
         crossOrigin="anonymous"
-      />
+      /> }
       {children}
-      <Script
-        strategy="lazyOnload"
-        src="https://www.googletagmanager.com/gtag/js?id=G-C3W52QVV6K"
-      />
-      <Script
-        id="google-analytics"
-        strategy="lazyOnload"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-C3W52QVV6K');
-          `,
-        }}
-      />
+      {isProduction && (
+        <>
+          <Script
+            strategy="lazyOnload"
+            src="https://www.googletagmanager.com/gtag/js?id=G-C3W52QVV6K"
+          />
+          <Script
+            id="google-analytics"
+            strategy="lazyOnload"
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-C3W52QVV6K');
+              `,
+            }}
+          />
+      </>)}
     </>
     )
 }
